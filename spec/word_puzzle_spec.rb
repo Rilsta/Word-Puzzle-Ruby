7d39e42df9e4e78ea2fd9071f9c7f555bedcae7c
riley <rstarnes9@gmail.com>
2016-01-08 08:58:19 -0800

    pass third and fourth spec dealing with capitalized characters and punctuation

--- spec/word_puzzle_spec.rb
+++ spec/word_puzzle_spec.rb
@@ -11,5 +11,15 @@ describe('String#word_puzzle') do
   end
   it('returns a word with the vowels subbed out as dash symbols') do
     expect("pizza".word_puzzle()).to(eq("p-zz-"))
+    expect("fenwick".word_puzzle()).to(eq("f-nw-ck"))
+    expect("beergarden".word_puzzle()).to(eq("b--rg-rd-n"))
+  end
+  it('deals with capitalized letters') do
+    expect("Oregon".word_puzzle()).to(eq("-r-g-n"))
+  end
+  it('deals with punctuation and symbols') do
+    expect("Yo-yo!".word_puzzle()).to(eq("y--y-!"))
+    expect("croissant?".word_puzzle()).to(eq("cr--ss-nt?"))
+    expect("!@A#%{b$%^e&*()g}".word_puzzle()).to(eq("!@-#%{b$%^-&*()g}"))
   end
 end
