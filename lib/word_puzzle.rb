ca8aefd842c14d7043459700b6f7345f1f2d0bda
riley <rstarnes9@gmail.com>
2016-01-08 08:04:22 -0800

    write first test and fix syntax issue in Gemfile

---
+++ lib/word_puzzle.rb
@@ -0,0 +1,5 @@
+class String
+  define_method(:word_puzzle) do
+    #code go here
+  end
+end
---
+++ spec/word_puzzle_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('word_puzzle')
+
+describe('String#word_puzzle') do
+  # it('returns a dash symbol when a letter is entered') do
+  #   expect("a".word_puzzle()).to(eq("-"))
+  end
+end
