42822b7e57af060d9a5f12157517e38846bdd50f
riley <rstarnes9@gmail.com>
2016-01-08 08:29:50 -0800

    pass first spec changing a single letter to the dash symbol

--- lib/word_puzzle.rb
+++ lib/word_puzzle.rb
@@ -1,5 +1,15 @@
 class String
   define_method(:word_puzzle) do
-    #code go here
+    vowels = ["a", "e", "i", "o", "u"]
+    old_sentence = self.split()
+    new_sentence = []
+    old_sentence.each()do |letter|
+      if vowels.include?(letter)
+        new_sentence.push("-")
+      else
+        new_sentence.push(letter)
+      end
+    end
+    new_sentence.join()
   end
 end
--- spec/word_puzzle_spec.rb
+++ spec/word_puzzle_spec.rb
@@ -2,7 +2,7 @@ require('rspec')
 require('word_puzzle')
 
 describe('String#word_puzzle') do
-  # it('returns a dash symbol when a letter is entered') do
-  #   expect("a".word_puzzle()).to(eq("-"))
+  it('returns a dash symbol when a letter is entered') do
+    expect("a".word_puzzle()).to(eq("-"))
   end
 end
